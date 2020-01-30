ed77a4e18a52498ded2b030c2af903ff065d5fda
Yosh <aisyar.arfah@gmail.com>
2020-01-30 17:14:02 +0700

    Forms Authentication in ASP.NET MVC

---
+++ ZERO/Controllers/AccountsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ZERO.Models;
+
+namespace ZERO.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User model)
+        {
+            using (ZEROEntities context = new ZEROEntities())
+            {
+                bool isValidUser = context.Users.Any(fl => fl.UserName.ToLower() == model.UserName.ToLower() && fl.UserPassword == model.UserPassword);
+                if (isValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+
+                ModelState.AddModelError("", "Invalid Username & Password");
+                return View();
+            }
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (ZEROEntities context = new ZEROEntities())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
