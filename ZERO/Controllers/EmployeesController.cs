d5bf6f652346421cceabc04d1b3d717e83befb0f
Yosh <aisyar.arfah@gmail.com>
2020-01-31 13:54:33 +0700

    Role-Based Authentication

--- ZERO/Controllers/EmployeesController.cs
+++ ZERO/Controllers/EmployeesController.cs
@@ -10,18 +10,19 @@ using ZERO.Models;
 
 namespace ZERO.Controllers
 {
-    [Authorize]
     public class EmployeesController : Controller
     {
         private ZEROEntities db = new ZEROEntities();
 
         // GET: Employees
+        [Authorize(Roles ="Admin, User, Customer")]
         public ActionResult Index()
         {
             return View(db.Employees.ToList());
         }
 
         // GET: Employees/Details/5
+        [Authorize(Roles = "Admin, Customer")]
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -37,6 +38,7 @@ namespace ZERO.Controllers
         }
 
         // GET: Employees/Create
+        [Authorize(Roles = "Admin, User")]
         public ActionResult Create()
         {
             return View();
@@ -45,6 +47,7 @@ namespace ZERO.Controllers
         // POST: Employees/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Admin, User")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ID,Name,Designation,Salary")] Employee employee)
@@ -60,6 +63,7 @@ namespace ZERO.Controllers
         }
 
         // GET: Employees/Edit/5
+        [Authorize(Roles = "User")]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -77,6 +81,7 @@ namespace ZERO.Controllers
         // POST: Employees/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "User")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ID,Name,Designation,Salary")] Employee employee)
